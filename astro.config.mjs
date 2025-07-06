951f0898001859c8cd91850a84a6892405b54e1d
Aji Pratama <atamasite@gmail.com>
2025-07-06 12:54:11 +0700

    fix: tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://aipedia.id',
   integrations: [
+    tailwind(),
     sitemap()
   ]
 });
